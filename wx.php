1367bb186a4443543d1b3ee6f1339487f944c2bb
zhanghao <zhanghao>
2017-06-05 11:35:45 +0800

    获取access_token值

--- wx.php
+++ wx.php
@@ -214,6 +214,56 @@ EOT;
             return false;
         }
     }
+
+    /*
+     * curl 请求 ， 返回数据
+     * */
+    public function getData($url) {
+        // 1. cURL初始化
+        $ch = curl_init();
+
+        // 2. 设置cURL选项
+        /*
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        */
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        // 3. 执行cURL请求
+        $ret = curl_exec($ch);
+
+        // 4. 关闭资源
+        curl_close($ch);
+
+        return $ret;
+    }
+
+    public function jsonToArray($json) {
+        $arr = json_decode($json, 1);
+        return $arr;
+    }
+
+    public function getAccesstoken() {
+
+        session_start();
+        if ($_SESSION['access_token'] && (time() - $_SESSION['expire_time']) < 7000 ) {
+        return $_SESSION['access_token'];
+        } else {
+            $appid = "wx499a9af85a5d4fe2";
+
+            $appsecret = "952ce89ced50797636b73b44b04efce0";
+
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+            $access_token = $this->jsonToArray($this->getData($url));
+            $_SESSION['access_token'] = $access_token;
+            $_SESSION['expire_time'] = time();
+            return $access_token;
+        }
+
+    }
 }
 
 ?>
